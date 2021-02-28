2999169c308f59d345676f9aecff4685479c20b2
Dzaky Rifai <76737872+dzaky-pr@users.noreply.github.com>
2021-02-28 12:44:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function open_letter() {
+    document.getElementsByClassName("letter-closed")[0].style.display = 'none';
+    document.getElementsByClassName("letter-open")[0].style.display = 'block';
+}
+
+function on_click_rtanny() {
+    alert('Yay! you found me!');
+    window.location.href='https://www.youtube.com/watch?v=1dcadBNZT00';
+}
